7ed1149c18e9f1f5846ab3f61784731c205c6751
ninjapretzel <ninjapretzel@yahoo.com>
2020-06-03 19:22:26 -0600

    Adds missing comments

--- Assets/Lab6/V2Scripts/OnBallisticProjectileHit.cs
+++ Assets/Lab6/V2Scripts/OnBallisticProjectileHit.cs
@@ -4,8 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Lab6 {
+
+	/// <summary> Class for attaching callbacks that can interrupt a <see cref="BallisticProjectile"/> during collision </summary>
 	public class OnBallisticProjectileHit : MonoBehaviour {
 
+		/// <summary> Callback. Should return false if the projectile should live, true if it should die. </summary>
 		public Func<BallisticProjectile, bool> OnHit = (proj => false);
 
 	}
